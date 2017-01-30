779062f1d59a52e2d6fda8fb0a126a79bd98123d
sbobylev <sbobylev@users.noreply.github.com>
2017-01-30 15:17:57 -0800

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello():
 	return "Your current server is " + socket.gethostname()
 
 if __name__ == '__main__':
-	app.run(debug=True,host='0.0.0.0',port=80)
+	app.run(debug=True,host='0.0.0.0',port=8080)
