9d3727f2c1ea35e5c5ef91f129c13f24d534039a
Bobylev, Stas <Stas.Bobylev@ca.com>
2017-11-25 14:40:41 -0800

    added client address

--- src/app.py
+++ src/app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 import socket
 app = Flask(__name__)
 
 @app.route('/')
 def host_info():
-	return "Your current server is " + socket.gethostname()
+	return jsonify(container_id=socket.gethostname(), 
+                       client_address=request.remote_addr)
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0',port=8080)
